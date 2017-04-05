2f2f649e83b1c3d1c44ac6e4b4a17ae4a2fc8576
haaalieee <morenoanalie14@gmail.com>
2017-04-05 14:33:58 +0800

    build

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($){
+(function($){
 	var timelineBlocks = $('.cd-timeline-block'),
 		offset = 0.8;
 
@@ -23,4 +23,4 @@ jQuery(document).ready(function($){
 			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		});
 	}
-});
+}());
